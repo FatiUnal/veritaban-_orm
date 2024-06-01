2ea5d99de3e3b86c3631f766027c4f8dba63f583
Fatih Unal <fatih_0226@outlook.com>
2024-06-01 10:42:15 +0300

    cross origin eklendi

--- src/main/java/org/example/orm/controller/DepartmantController.java
+++ src/main/java/org/example/orm/controller/DepartmantController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/departmant")
 public class DepartmantController {
